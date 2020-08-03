36b5bba953ffbce893a34479ede37cd273e0a9e1
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-02 20:43:37 -0700

    stay or go control flow

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,18 @@ var fight = function() {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+// if player choses to fight, then fight
+if (promptFight === "fight" || promptFight === "FIGHT") {
+
     // Subtract the value of 'playerAttack; from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
     // check enemy's health
     if (enemyHealth <= 0) {
@@ -24,11 +34,6 @@ var fight = function() {
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
     playerHealth = playerHealth - enemyAttack;
@@ -45,6 +50,13 @@ var fight = function() {
     else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
+
+    // if player choses to skip
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+} else {
+    window.alert("You need to pick a valid option. Try again!");
+}
 };
 
 // execute function
