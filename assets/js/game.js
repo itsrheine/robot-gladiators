61ef8c4bd35586b7bf960c24988977b31baba897
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-03 18:30:37 -0700

    combatant using function arguments

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,23 +3,9 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-
-// Game States
-console.log (enemyNames);
-enemyNames [0];
-console.log (enemyNames.length);
-
-for (var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
 
 // "WIN" - Player robot has defeated all enemy robots
 //   * Fight all enemy robots
@@ -89,5 +75,6 @@ var fight = function() {
 };
 
 // execute function
-//fight();
-
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
